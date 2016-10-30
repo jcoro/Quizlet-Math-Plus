77987cbc02e18529ce4d09e58fffe7a667d40433
John <jcoronite@hotmail.com>
2016-10-29 21:31:12 -0700

    feat: Add MathView Library
    Add Add MathView Library to render mathematical
    formulas.

--- app/src/main/java/net/coronite/quizlet_math_plus/DetailActivityFragment.java
+++ app/src/main/java/net/coronite/quizlet_math_plus/DetailActivityFragment.java
@@ -1,10 +1,11 @@
 package net.coronite.quizlet_math_plus;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,6 +19,23 @@ public class DetailActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        final Button button = (Button)view.findViewById(R.id.answer_button);
+        final View answer = view.findViewById(R.id.answer);
+        answer.setVisibility(View.INVISIBLE);
+        button.setText(R.string.show_answer);
+        button.setOnClickListener(new View.OnClickListener(){
+        @Override
+            public void onClick(View v){
+                if(button.getText().equals(getText(R.string.show_answer))){
+                    button.setText(R.string.hide_answer);
+                    answer.setVisibility(View.VISIBLE);
+                } else {
+                    button.setText(R.string.show_answer);
+                    answer.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
         return view;
     }
 }
